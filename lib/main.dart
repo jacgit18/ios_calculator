78aceb2c8e2153658fcae03a62eeaa13810c846b
jacgit18 <joshuacarpentier21@gmail.com>
2019-12-04 18:59:32 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Joshua Carpentier - Tip Calculator'),
+      home: MyHomePage(title: 'Joshua Carpentier - Grade Calculator'),
     );
   }
 }
@@ -29,13 +29,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String t1, t2, result1 = '';
-  double n2;
-  TipCalculate fish = TipCalculate(service: 'g', bill: 100);
   void _incrementCounter() {
     setState(() {
       _counter++;
-      print('t1 =' + t1 + '. t2 = ' + t2);
-      result1 = fish.tip(n2, t1);
     });
   }
 
@@ -47,65 +43,62 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(result1, style: TextStyle(fontSize: 22.9) ,),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text('Result Goes here',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'courier',
+                        backgroundColor: Colors.blue,
+                        color: Colors.yellow,
+                    ),
+
+
+                  ),
+                ),
+              ],
+            ),
+          SizedBox(height: 10.0,),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Enter your Name Please'),
+            keyboardType: TextInputType.text,
+            onChanged: (text){
+              t1 = text;
+            },
+          ),
+            SizedBox(height: 20.0,),
             TextField(
-              keyboardType: TextInputType.text,
               decoration: InputDecoration(
-                  labelText: 'Enter Service',
                   border: OutlineInputBorder(),
-                  hintText: 'good(g), alright(a), not bad(n), poor(p)'
-              ),
-              onChanged:  (text){
-                t1 = text;
-                //n2 = double.parse(t2);
+                  labelText: 'Enter your Grade Please'),
+              keyboardType: TextInputType.text,
+              onChanged: (text){
+                t2 = text;
               },
             ),
-            SizedBox(height: 40,),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'Enter the bill please',
-                  border: OutlineInputBorder(),
-                  hintText: 'How much was the bill?'
+
+          FlatButton(onPressed: _incrementCounter,
+              child: Container(margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                
+                child: Text('Find your grade', textAlign:  TextAlign.start,),
+                decoration: BoxDecoration(color: Colors.green,
+                    border: Border.all()),
               ),
-              onChanged:  (text){
-                t2 = text;
-                n2 = double.parse(t2);
-              },
-            )
+
+              ),
 
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Find Tip',
-        child: Icon(Icons.timer),
-      ),
+
     );
   }
 }
 
-class TipCalculate{
-  double bill, tiprate;
-  String service, result="";
-  TipCalculate({this.bill, this.service});
-  String tip(bill, service){
-    if(service == 'g' || service == 'good')
-      tiprate = 0.25;
-    else if(service == 'a' || service == 'alright')
-      tiprate = 0.15;
-    else if(service == 'n' || service == 'not bad')
-      tiprate = 0.10;
-    else if(service == 'p' || service == 'poor')
-      tiprate = 0;
-
-    result = 'Tip is \$' + (bill * tiprate).toString() + '.';
-    return result;
-  }
-
-}
